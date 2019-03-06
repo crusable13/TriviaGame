04b4bd076ecfeaa9bb91370e8ba3509d20eafe04
Christopher <jcelitesmith@gmail.com>
2019-03-06 11:56:23 -0800

    Questions Array Created

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+var questions = [ {
+    q: "Gordon Ramsay used to be a soccer player?",
+    a: true
+}, {
+    q: "Django Unchained won best supporting actor?",
+    a: true
+}, {
+    q: "America is a state sponsor of Terrorism?",
+    a: true
+}, {
+    q: "The Earth is flat?",
+    a: false
+}, {
+    q: "Christianity is the source of all good in the western world?",
+    a: false
+}, {
+    q: "The first amendment gives you the RIGHT TO BEAR ARMS?",
+    a: false
+}, {
+    q: "China hacked the 2016 election?",
+    a: false
+}, {
+    q: "The new Game of Thrones season is over hyped?",
+    a: true
+}, {
+    q: "Freedom is free?",
+    a: false
+}, {
+    q: "Karl Marx was a Stalinist Marxist?",
+    a: false
+}, {
+    q: "South Africa is a dying Nation?",
+    a: false
+}
+]
+
+console.log(questions[10].a)
+
