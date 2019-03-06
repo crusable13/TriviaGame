68594a5996e8cf445960526bf13a8f0a74edc34e
Christopher <jcelitesmith@gmail.com>
2019-03-06 13:59:02 -0800

    Working game with no styiling. Minor bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var questions = [ {
+const questions = [{
     q: "Gordon Ramsay used to be a soccer player?",
     a: true
 }, {
@@ -39,8 +39,10 @@ let userAnswer = "";
 var winCounter = 0;
 var lossCounter = 0;
 
-function questionDisplay(array, i) {
-    let question = array[i].q;
+function questionDisplay(i) {
+    console.log(questions[i])
+    let question = questions[i].q;
+    
     $("#questionDiv").append(question)
 }
 
@@ -48,34 +50,71 @@ function isTrue(string) {
     if (string == "t") {
         console.log("true");
         return true;
-        
+
     } else if (string == "f") {
-       console.log("false"); 
-       return false;
-        
+        console.log("false");
+        return false;
+
     }
 }
-function questionPicker(min,max) 
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function questionPicker(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-$("#startButton").on("click", function(event){
-        startGame();
-    })
+function checkAnswer(ans, i) {
+    if(ans == questions[i].a) {
+        console.log(ans)
+        console.log("one more win!");
+        winCounter++;
+        
+    } else if (ans !== questions[i].a) {
+        console.log(ans)
+        console.log(questions[i].a)
+        console.log("one more loss!");
+        lossCounter++;
+        
+    }
+    $("#questionDiv").empty();
 
+    triviaRound();
+}
+$("#startButton").on("click", function (event) {
+    startGame();
+})
 
+function triviaRound() {
 
+    let l = questionPicker(0, questions.length - 1);
+
+    questionDisplay(l)
+
+    document.onkeyup = function (event) {
+        userAnswer = event.key;
+        console.log(userAnswer);
+        userAnswer = isTrue(userAnswer);
+        checkAnswer(userAnswer, l);
+    }
+
+    
+}
+
+function endGame() {
+    $("#answers").append("Correct Answers: " + winCounter);
+    $("#answers").append("Wrong Answers: " + lossCounter);
+    $("#questionDiv").text("Care to play again? Just hit the button!");
+
+    
+    $("#startButton").show();
+
+}
 function startGame() {
-    // setTimeout(endGame, 1000 * 60);
-    console.log("yous started the game!")
+    setTimeout(endGame, 1000 * 10);
+    
+    console.log("you started the game!")
     $("#startButton").hide();
 
-    questionDisplay(questions, questionPicker(0,questions.length))
+    
+    triviaRound();
+    
 }
 
-document.onkeyup = function(event) {
-    userAnswer = event.key;
-    console.log(userAnswer);
-    isTrue(userAnswer)
-}
